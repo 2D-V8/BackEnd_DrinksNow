36fafa769962c06dac07772ccbd8b1d982ce7647
2D-V8 <danielvalenccia09@gmail.com>
2025-05-20 23:47:16 -0500

    Changed dotenv congiguration

--- server.js
+++ server.js
@@ -1,48 +1,61 @@
-require('dotenv').config(); // Cargar variables del .env
-import express, { json, urlencoded } from 'express';
+const express = require('express');
 const app = express();
-import { createServer } from 'http';
-const server = createServer(app);
-import logger from 'morgan';
-import cors from 'cors';
-import passport, { initialize, session } from 'passport';
-import multer, { memoryStorage } from 'multer';
+const http = require('http');
+const server = http.createServer(app);
+const logger = require('morgan');
+const cors = require('cors');
+const passport = require ('passport');
+const multer = require('multer')
+/*
+Importamos Rutas
+*/
+const userRoutes = require('./routes/userRoutes');
 
-// Importar rutas
-import userRoutes from './routes/userRoutes';
 
 const port = process.env.PORT || 3000;
 
-// Middlewares
+// función que se ejecuta antes de la petición 
 app.use(logger('dev'));
-app.use(json()); 
-app.use(urlencoded({ extended: true }));
+app.use(express.json()); 
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-app.use(initialize());
-app.use(session());
+app.use(passport.initialize());
+app.use(passport.session());
+
 require('./config/passport')(passport);
+
 app.disable('x-powered-by');
 
-const upload = multer({ storage: memoryStorage() });
+// Puerto
+app.set('port', port);
 
-// Rutas
+unload = multer({
+  storage: multer.memoryStorage()
+
+});
+/*
+Llamado Rutas
+*/
 userRoutes(app);
 
+
 // Iniciar servidor
-server.listen(port, () => {
-  console.log(`Servidor Node.js escuchando en puerto ${port}`);
+server.listen(port, '192.168.1.15', () => {
+  console.log(`Aplicación de NodeJs iniciada en el puerto ${port}`);
 });
 
-// Rutas básicas
+// Ruta raíz
 app.get('/', (req, res) => {
   res.send('Ruta Raiz BackEnd');
 });
+
+// Ruta test
 app.get('/test', (req, res) => {
-  res.send('Ruta test');
-});
+    res.send('Ruta test');
+  });
 
 // Manejo de errores
 app.use((err, req, res, next) => {
-  console.error(err);
+  console.log(err);
   res.status(err.status || 500).send(err.stack);
 });
